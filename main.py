d68399d4a02fd5d5852cbfdc8305eb63df57236f
Shinhoo Kim <54264875+KSH-KOR@users.noreply.github.com>
2023-08-14 14:34:06 +0900

    api

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+import asyncio
 
 from api.hotel import hotel_router
 from api.index import app_router
@@ -10,4 +11,5 @@ app.include_router(hotel_router)
 app.include_router(app_router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(uvicorn.run(app, host="0.0.0.0", port=8001))
